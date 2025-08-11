571ada03d28d6ca42b06e76a7a503baaac48b8ed
Marcelo Coldibelli <marcelo.coldibelli@gmail.com>
2025-08-11 11:52:45 -0300

    fix(domain): corrects method typo

--- apps/api/monitor/src/main/java/com/nlphd/monitor/sales/domain/SalesOrderRepository.java
+++ apps/api/monitor/src/main/java/com/nlphd/monitor/sales/domain/SalesOrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SalesOrderRepository {
-  List<SalesOrder> findAll(int limit);
+  List<SalesOrder> findRecent(int limit);
 }
